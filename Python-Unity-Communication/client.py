26eb6017bcc79b9f0f5a80707dad5f6b46279a75
MaOliX <matheus21st@gmail.com>
2019-04-02 15:44:21 -0300

    commented

--- Python-Unity-Communication/client.py
+++ Python-Unity-Communication/client.py
@@ -1,18 +1,26 @@
+# it's important to run the server before the client
+
 import socket
 
+#localhost adress, the server host
 host = '127.0.0.1'
+#port you will use for the server communication, use comething with 5 digits to avoid conflicts, like 12345
 port = 12345
-data = 0
+
+#create socket object
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 print("IP Adress: " +host)
 print("Port: "+str(port))
 
+#connect to the server. The server has to accept the connection.
 s.connect((host,port))
 
 while True:
+	#if you want to exit the loop and end the software
 	if input("->")=="exit":
 		break;
-	datasent=str.encode(input("->"))
+	#data you want to send to the server you are connected
+	datasent=str.encode(input("->")) #the data need to be encoded
 	s.send(datasent)
 		#print(sent)
--- Python-Unity-Communication/server.py
+++ Python-Unity-Communication/server.py
@@ -1,18 +1,27 @@
+# it's important to run the server before the client
+
 import socket
 
+#server and client adress(host,port) need to be the same
+#localhost adress
 host = '127.0.0.1'
+#port you will use for the server communication, use comething with 5 digits to avoid conflicts, like 12345
 port = 12345
+
 data = 1111
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 print("IP Adress: " +host)
 print("Port: "+str(port))
 
+#create the socket object
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
+#this line above make this code the server
 s.bind((host,port))
+#Number of connected clients, I put 5, but i think 1 works too. We only need one client.
 s.listen(5)
 
+#now we enter in this loop until the client try to connect. So we accept.
 conn = None
 
 while conn == None:
@@ -20,14 +29,15 @@ while conn == None:
 		conn, addr = s.accept()
 	except:
 		pass
-
+#test
 print(conn)
 print(addr)
 
+#main loop
 while True:
 	
 	try:
-		data = conn.recv(2048)
+		data = conn.recv(2048) #get the data from the client
 		print(data)
 	except Exception as e: 
 		print(e)
